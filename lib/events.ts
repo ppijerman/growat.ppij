e93d907c3775549b1d18b593ff2f6d549e03e7c4
BertsQ <bertsq@bertsquilandio.com>
2025-02-16 11:49:10 +0100

    changing slug to genAI-workshop

--- lib/events.ts
+++ lib/events.ts
@@ -14,8 +14,8 @@ type Event = {
 
 export const events: Event[] = [
   {
-    slug: "aipse-workshop",
-    title: "AIPSE Workshop",
+    slug: "genAI-workshop",
+    title: "GenAI Workshop",
     description: "AI stuff",
     date: "2022-01-01",
     location: "Online",
