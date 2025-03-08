b9d1267277ffb25a84faa30389f1bb346aca6338
Ariyosena <ariyosena.sutandang@campus.tu-berlin.de>
2025-03-08 22:34:32 +0100

    countdown done

--- app/(pages)/events/step-up-mentorship/components/countdown.tsx
+++ app/(pages)/events/step-up-mentorship/components/countdown.tsx
@@ -34,22 +34,34 @@ export function Countdown() {
   }, [eventDate]);
 
   return (
-    <section className="flex flex-col justify-center items-center text-primary bg-secondary min-h-[30rem] py-5">
-      <div className="grid grid-cols-2 md:grid-cols-4 items-center w-full lg:w-1/2 py-5">
+    <section className="flex flex-col justify-center items-center text-primary bg-ga-light-green min-h-[25rem] py-5">
+      <div className="flex flex-col text-ga-green w-full px-18 max-w-6xl mx-auto">
+        <h1 className="m-0 text-1xl sm:text-1xl md:text-3xl lg:text-4xl text-left">Get Ready</h1>
+        <div className="w-full flex justify-center">
+          <div className="w-full max-w-6xl">
+            <h1 className="m-0 text-left text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold tracking-wide">
+              Your Mentorship Journey Begins Soon!
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 items-center w-3/4 lg:w-4/5 py-5">
         {Object.entries(countdown).map(([key, value]) => (
-          <div key={key} className="flex flex-col items-center space-y-1">
+          <div key={key} className="flex flex-col items-center">
             <strong
               aria-label={key}
-              className="text-3xl md:text-4xl lg:text-5xl font-bold text-accent"
+              className="text-7xl md:text-9xl lg:text-11xl font-bold text-ga-blue"
             >
               {value}
             </strong>
-            <span className="text-xl md:text-2xl lg:text-3xl font-medium">
+            <span className="text-xl md:text-2xl lg:text-3xl font-bold text-ga-green">
               {key.toUpperCase()}
             </span>
           </div>
         ))}
       </div>
     </section>
+
   );
 }
