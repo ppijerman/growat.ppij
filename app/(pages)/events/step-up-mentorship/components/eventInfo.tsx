9c29d03bece59da247cc58818394952fecea84c7
Muhammad Daryl Rashad <darylrashad@gmail.com>
2025-03-24 00:13:59 +0100

    refactoring and mentorship timeline component

--- app/(pages)/events/step-up-mentorship/components/eventInfo.tsx
+++ app/(pages)/events/step-up-mentorship/components/eventInfo.tsx
@@ -1,13 +1,13 @@
 "use client"
-import Image from 'next/image'
-import teamworkPhoto from '@/public/assets/teamwork.jpg'
+import Image from "next/image"
+import teamworkPhoto from "@/public/assets/teamwork.jpg"
 
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
-  CardDescription
+  CardDescription,
 } from "@/components/ui/card"
 import React, { ReactElement } from "react"
 
@@ -27,7 +27,7 @@ import {
 } from "lucide-react"
 
 const iconClasses =
-  "w-[50px] h-[50px] sm:w-[70px] sm:h-[70px] md:w-[80px] md:h-[80px] lg:w-[80px] lg:h-[80px]"
+  "w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[100px] md:h-[100px] lg:w-[110px] lg:h-[110px]"
 
 const sessionData: CardData[] = [
   {
@@ -44,7 +44,7 @@ const sessionData: CardData[] = [
     title: "Webinar-like Summits",
     desc: "Diadakan setiap hari Sabtu, membahas tema-tema penting terkait karier.",
     icon: <Laptop className={iconClasses} />,
-  }
+  },
 ]
 
 const involveData: CardData[] = [
@@ -62,90 +62,101 @@ const involveData: CardData[] = [
     title: "Summit Speaker",
     desc: "Pembicara dengan latar belakang kuat di bidang career building, yang berbagi wawasan berharga selama summit.",
     icon: <Mic2Icon className={iconClasses} />,
-  }
+  },
 ]
 
 export default function EventInfo() {
   return (
     <main>
       <div className="md:flex lg:col-2 lg:row-1">
-        <div className="lg:w-1/2" style={{position:"relative"}}>
-          <Image src={teamworkPhoto} alt="photo" fill={true} style={{objectFit: "cover"}} placeholder = 'blur'/>
+        <div className="lg:w-1/2" style={{ position: "relative" }}>
+          <Image
+            src={teamworkPhoto}
+            alt="photo"
+            fill={true}
+            style={{ objectFit: "cover" }}
+            placeholder="blur"
+          />
         </div>
 
         <div className="lg:w-1/2 w-full">
           <div className="flex bg-gradient-to-r from-ga-blue to-[#a2d3bc] pl-3 sm:p-3 h-1/4 items-center justify-center">
-            <h1 className="text-wrap text-3xl lg:text-5xl text-ga-beige font-bold pt-3">What is Step Up <br/> Mentorship 3.0</h1>
+            <h1 className="text-wrap text-3xl lg:text-5xl text-ga-light font-bold py-3">
+              What is Step Up Mentorship 3.0?
+            </h1>
           </div>
 
           <div className="flex flex-col items-center justify-center p-5 bg-ga-light h-3/4">
-            <p className='leading-7 lg:pt-5 lg:mb-5 sm:text-lg lg:text-2xl'>
-              <b>Step Up Mentorship 3.0</b> adalah program <i>mentoring</i> intensif GRATIS yang berlangsung selama 3 minggu, 
-              yang mencakup berbagai kegiatan, seperti <i>summit</i>, sesi <i>group mentoring</i>, dan sesi <i>one-on-one</i> dengan profesional dari berbagai industri.
+            <p className="leading-7 lg:pt-5 lg:mb-5 sm:text-lg lg:text-2xl">
+              <b>Step Up Mentorship 3.0</b> adalah program <i>mentoring</i>{" "}
+              intensif GRATIS yang berlangsung selama 3 minggu, yang mencakup
+              berbagai kegiatan, seperti <i>summit</i>, sesi{" "}
+              <i>group mentoring</i>, dan sesi <i>one-on-one</i> dengan
+              profesional dari berbagai industri.
             </p>
-            <p className='leading-7 sm:text-lg lg:text-2xl'>
-            <b>Tujuan kami</b> <br/>Membantu dan mendampingi mahasiswa Indonesia di Jerman untuk memperoleh <b>wawasan</b>, 
-            sehingga dapat <b>berkembang</b> menjadi <b>profesional yang percaya diri</b> dan <b>siap menghadapi dunia kerja</b>.
+            <p className="leading-7 sm:text-lg lg:text-2xl">
+              <b>Tujuan kami</b> <br />
+              Membantu dan mendampingi mahasiswa Indonesia di Jerman untuk
+              memperoleh <b>wawasan</b>, sehingga dapat <b>berkembang</b>{" "}
+              menjadi <b>profesional yang percaya diri</b> dan{" "}
+              <b>siap menghadapi dunia kerja</b>.
             </p>
           </div>
         </div>
-        
       </div>
 
       {/* what do we offer */}
       <div>
         <div className="bg-ga-light-green p-5">
-          <h1 className="text-3xl leading-6 font-bold lg:text-5xl lg:leading-11">
+          <h1 className="text-3xl leading-6 font-bold lg:text-5xl lg:leading-11 py-3">
             What do we offer?
           </h1>
         </div>
         {/* Card */}
         <div className="flex flex-wrap md:flex-nowrap items-center justify-center md:gap-4 l:gap-8 xl:gap-14 md:py-5 md:px-14 l:px-22 xl:px-30 md:bg-ga-light">
-        {sessionData.map((item, index) => (
-          <Card key={index} 
-          className="flex flex-col sm:w-full md:w-1/3 bg-gradient-to-r from-ga-blue to-[#a2d3bc] h-fit md:h-96 border-0 rounded-none md:rounded-2xl"
-          >
-            <CardHeader className="items-center text-center text-ga-light">
-              <CardContent>
-                {item.icon}
-              </CardContent>
-              <CardTitle className="text-lg md:text-xl lg:text-2xl font-bold">
-                {item.title}
-              </CardTitle>
-            </CardHeader>
-            <CardDescription className="self-center text-justify text-md sm:text-lg lg:text-xl px-10 md:px-4 lg:px-5 pb-4 text-ga-light">
-              {item.desc}
-            </CardDescription>
-          </Card>
-        ))}
+          {sessionData.map((item, index) => (
+            <Card
+              key={index}
+              className="flex flex-col sm:w-full md:w-1/3 bg-gradient-to-tr from-ga-blue from-40% to-[#a2d3bc] h-fit md:h-96 border-0 rounded-none md:rounded-2xl"
+            >
+              <CardHeader className="items-center text-center text-ga-light">
+                <CardContent>{item.icon}</CardContent>
+                <CardTitle className="text-lg md:text-xl lg:text-2xl font-bold">
+                  {item.title}
+                </CardTitle>
+              </CardHeader>
+              <CardDescription className="self-center text-justify text-md sm:text-lg lg:text-xl px-10 md:px-4 lg:px-5 pb-4 text-ga-light">
+                {item.desc}
+              </CardDescription>
+            </Card>
+          ))}
         </div>
       </div>
 
       {/* who will be involved */}
 
       <div className="bg-ga-green p-5 pt-10">
-          <h1 className="text-3xl leading-6 font-bold text-ga-light lg:text-5xl lg:leading-11">
-            <i>Who will be involved?</i>
-          </h1>
-          {/* Card */}
-        <div className="flex flex-wrap md:flex-nowrap items-center justify-center md:gap-4 l:gap-8 xl:gap-14 md:py-5 md:px-14 l:px-22 xl:px-30 bg-ga-green">
-        {involveData.map((item, index) => (
-          <Card key={index} 
-          className="flex flex-col sm:w-full md:w-1/3 bg-ga-green h-fit md:h-96 border-0 rounded-none md:rounded-2xl"
-          >
-            <CardHeader className="items-center text-center text-ga-light">
-              <CardContent>
-                {item.icon}
-              </CardContent>
-              <CardTitle className="text-lg md:text-xl lg:text-2xl font-bold">
-                {item.title}
-              </CardTitle>
-            </CardHeader>
-            <CardDescription className="self-center text-justify text-md sm:text-lg lg:text-xl px-10 md:px-4 lg:px-5 pb-4 text-ga-light">
-              {item.desc}
-            </CardDescription>
-          </Card>
-        ))}
+        <h1 className="text-3xl leading-6 font-bold text-ga-light lg:text-5xl lg:leading-11">
+          <i>Who will be involved?</i>
+        </h1>
+        {/* Card */}
+        <div className="flex flex-wrap md:flex-nowrap items-start justify-center md:gap-4 l:gap-8 xl:gap-14 md:py-5 md:px-14 l:px-22 xl:px-30 bg-ga-green">
+          {involveData.map((item, index) => (
+            <Card
+              key={index}
+              className="flex-col sm:w-full md:w-1/3 bg-ga-green border-0 shadow-none rounded-none md:rounded-2xl"
+            >
+              <CardHeader className="items-center text-center text-ga-light">
+                <CardContent>{item.icon}</CardContent>
+                <CardTitle className="text-lg md:text-xl lg:text-2xl font-bold">
+                  {item.title}
+                </CardTitle>
+              </CardHeader>
+              <CardDescription className="self-center text-center text-md sm:text-lg lg:text-xl px-10 md:px-4 lg:px-5 text-ga-light">
+                {item.desc}
+              </CardDescription>
+            </Card>
+          ))}
         </div>
       </div>
     </main>
--- app/(pages)/events/step-up-mentorship/components/hero.tsx
+++ app/(pages)/events/step-up-mentorship/components/hero.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image"
-// import Link from "next/link"
 
 import logoPPIJ from "@/public/assets/logoppij.png"
 import logoGrowAt from "@/public/assets/logogrowat.png"
-
-const buttonClass = "bg-ga-green hover:cursor-pointer hover:bg-ga-green/80 text-ga-light py-2 px-4 rounded drop-shadow-xl shadow-ga-green/80"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Hero() {
   return (
@@ -12,9 +11,9 @@ export default function Hero() {
       <div className="bg-[url('/assets/herosum.png')] bg-cover bg-center bg-no-repeat pb-10">
         <div className="flex items-center py-10 px-5 lg:px-40 xl:px-72">
           {/* logo growat & ppij */}
-          <div className="flex py-5 px-5 mx-auto">
-            <Image src={logoGrowAt} alt="logo growat" height={20}/>
-            <Image src={logoPPIJ} alt="logo PPIJ" height={20}/>
+          <div className="flex py-5 px-5 mx-auto opacity-0">
+            <Image src={logoGrowAt} alt="logo growat" height={20} />
+            <Image src={logoPPIJ} alt="logo PPIJ" height={20} />
           </div>
         </div>
         <div className="text-ga-green">
@@ -24,16 +23,21 @@ export default function Hero() {
           <h2 className="text-center pt-6 text-2xl lg:text-2xl lg:leading-7">
             19 April - 10 Mei 2025 | Online
           </h2>
-          <div className="flex flex-wrap items-center justify-center gap-6 pt-8 px-5 md:mx-14 lg:px-30 lg:mx-30 lg:gap-x-20 xl:mx-72">
-            <button className={buttonClass}>
+          <div className="flex flex-wrap items-center justify-center gap-6 pt-8 md:mx-14 lg:px-30 lg:mx-30 lg:gap-x-20 xl:mx-72">
+            {/* <button className={buttonClass}>
               Mentors: Start Here!
             </button>
             <button className={buttonClass}>
               Get your handbook here!
-            </button>
-            <button className={buttonClass}>
-              Mentees: Register Here!
-            </button>
+            </button> */}
+            <Button variant="secondary" className="" asChild>
+              <Link
+                href="https://ppij.org/StepUp-MenteeRegistForm "
+                className="text-xl py-5 px-4"
+              >
+                Mentees: Register here!
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
