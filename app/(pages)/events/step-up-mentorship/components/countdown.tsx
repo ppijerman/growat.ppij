38a673f53b630f4048f8a94fbc768a375bd2f4fa
Muhammad Daryl Rashad <darylrashad@gmail.com>
2025-04-07 01:49:52 +0200

    Refactor and update step up countdown component

--- app/(pages)/events/step-up-mentorship/components/countdown.tsx
+++ app/(pages)/events/step-up-mentorship/components/countdown.tsx
@@ -35,26 +35,26 @@ export function Countdown() {
   }, [])
 
   return (
-    <section className="flex flex-col justify-center items-center text-primary bg-ga-light-green min-h-[25rem] py-5">
+    <section className="flex flex-col justify-center items-center text-primary bg-ga-light-green py-5">
       <div className="flex flex-col text-ga-green w-full px-18 max-w-6xl mx-auto">
         <h1 className="m-0 text-1xl sm:text-1xl md:text-3xl lg:text-4xl text-left">
-          Get Ready
+          Your Mentorship Journey Begins Soon!
         </h1>
         <div className="w-full flex justify-center">
-          <div className="w-full max-w-6xl">
-            <h1 className="m-0 text-left text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold tracking-wide">
-              Your Mentorship Journey Begins Soon!
+          <div className="w-full max-w-5xl">
+            <h1 className="m-0 text-left text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold tracking-wide">
+              Registration closes in
             </h1>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 items-center w-3/4 lg:w-4/5 py-5">
+      <div className="grid grid-cols-2 md:grid-cols-4 items-center gap-5 lg:gap-10 py-5">
         {Object.entries(countdown).map(([key, value]) => (
           <div key={key} className="flex flex-col items-center">
             <strong
               aria-label={key}
-              className="text-7xl md:text-9xl lg:text-11xl font-bold text-ga-blue"
+              className="text-6xl md:text-8xl lg:text-10xl font-bold text-ga-blue"
             >
               {value}
             </strong>
