63aac14baf84014c959c01377927a8c6d8baf58c
reyna isabel <reyna.kurniawan@gmail.com>
2025-04-07 12:57:49 +0200

    edited countdown, modified mentors page

--- app/(pages)/events/step-up-mentorship/components/countdown.tsx
+++ app/(pages)/events/step-up-mentorship/components/countdown.tsx
@@ -35,30 +35,30 @@ export function Countdown() {
   }, [])
 
   return (
-    <section className="flex flex-col justify-center items-center text-primary bg-ga-light-green min-h-[25rem] py-5">
+    <section className="flex flex-col justify-center items-center text-primary bg-ga-light-green min-h-[16rem] py-5">
       <div className="flex flex-col text-ga-green w-full px-18 max-w-6xl mx-auto">
-        <h1 className="m-0 text-1xl sm:text-1xl md:text-3xl lg:text-4xl text-left">
+        <h1 className="m-0 text-md sm:text-lg md:text-xl lg:text-2xl text-center">
           Get Ready
         </h1>
         <div className="w-full flex justify-center">
           <div className="w-full max-w-6xl">
-            <h1 className="m-0 text-left text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold tracking-wide">
+            <h1 className="m-0 text-center text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold tracking-wide">
               Your Mentorship Journey Begins Soon!
             </h1>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 items-center w-3/4 lg:w-4/5 py-5">
+      <div className="grid grid-cols-2 md:grid-cols-4 items-center w-1/2 lg:w-1/2 py-5">
         {Object.entries(countdown).map(([key, value]) => (
           <div key={key} className="flex flex-col items-center">
             <strong
               aria-label={key}
-              className="text-7xl md:text-9xl lg:text-11xl font-bold text-ga-blue"
+              className="text-5xl md:text-6xl lg:text-11xl font-bold text-ga-blue"
             >
               {value}
             </strong>
-            <span className="text-xl md:text-2xl lg:text-3xl font-bold text-ga-green">
+            <span className="text-md md:text-lg lg:text-xl font-bold text-ga-green">
               {key.toUpperCase()}
             </span>
           </div>
--- public/assets/stepUpMentors/mentors.ts
+++ public/assets/stepUpMentors/mentors.ts
@@ -1,38 +1,115 @@
 
-import placeholderPic from "@/public/assets/img-round-placeholder.png";
+import MentorPic from "@/public/assets/img-round-placeholder.png";
 
 export const Mentors = {
-  placeHolder: {
-    name: "Name here",
+  Mentor: {
+    name: "Adji Pramudito Arioputro",
     title: "Title here",
-    image: placeholderPic,
+    image: MentorPic,
   },
-  placeHolder2: {
-    name: "Name here",
+  Mentor2: {
+    name: "Arsyatria Rasaki Akbar",
     title: "Title here",
-    image: placeholderPic,
+    image: MentorPic,
   },
-  placeHolder3: {
-    name: "Name here",
+  Mentor3: {
+    name: "Diaddra Pramudito",
     title: "Title here",
-    image: placeholderPic,
+    image: MentorPic,
   },
-  placeHolder4: {
-    name: "Name here",
+  Mentor4: {
+    name: "Stephen Lowis Putra",
     title: "Title here",
-    image: placeholderPic,
+    image: MentorPic,
   },
-  placeHolder5: {
-    name: "Name here",
+  Mentor5: {
+    name: "Gita Flowerensia Anandini",
     title: "Title here",
-    image: placeholderPic,
+    image: MentorPic,
+  },
+  Mentor6: {
+    name: "Sridanto Farras Rachmadi",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor7: {
+    name: "Safrina Sucipto",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor8: {
+    name: "Rizky I. Sugianto",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor9: {
+    name: "Tiara Cahaya Putri",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor10: {
+    name: "Nabil Akbarazzima Fatih",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor11: {
+    name: "Citra Puspita Maharani",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor12: {
+    name: "Ainur Rosyidah (Ochi)",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor13: {
+    name: "Naufal Ekadyanto Nugroho",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor14: {
+    name: "Livia Marcha",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor15: {
+    name: "Philia Magdalena Setiawan",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor16: {
+    name: "Tobias Kusnaman",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor17: {
+    name: "Kevin",
+    title: "Title here",
+    image: MentorPic,
+  },
+  Mentor18: {
+    name: "Gerry Yota",
+    title: "Title here",
+    image: MentorPic,
   },
 };
 
 export const MentorsList = [
-  Mentors.placeHolder,
-  Mentors.placeHolder2,
-  Mentors.placeHolder3,
-  Mentors.placeHolder4,
-  Mentors.placeHolder5,
+  Mentors.Mentor,
+  Mentors.Mentor2,
+  Mentors.Mentor3,
+  Mentors.Mentor4,
+  Mentors.Mentor5,
+  Mentors.Mentor6,
+  Mentors.Mentor7,
+  Mentors.Mentor8,
+  Mentors.Mentor9,
+  Mentors.Mentor10,
+  Mentors.Mentor11,
+  Mentors.Mentor12,
+  Mentors.Mentor13,
+  Mentors.Mentor15,
+  Mentors.Mentor16,
+  Mentors.Mentor17,
+  Mentors.Mentor18,
 ];
