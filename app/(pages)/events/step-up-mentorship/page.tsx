1f436e481886a7f20dcc9864a116766c9bab4949
Muhammad Daryl Rashad <darylrashad@gmail.com>
2025-04-15 18:36:40 +0200

    comment out import countdown step up

--- app/(pages)/events/step-up-mentorship/page.tsx
+++ app/(pages)/events/step-up-mentorship/page.tsx
@@ -1,7 +1,7 @@
 import EventInfo from "./components/eventInfo"
 import Timeline from "./components/timeline"
 import Hero from "./components/hero"
-import { Countdown } from "./components/countdown"
+// import { Countdown } from "./components/countdown"
 import Mentors from "./components/mentors"
 
 export default function Page() {
